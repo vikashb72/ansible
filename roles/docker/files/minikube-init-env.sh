1a9ebb7ac6d6b204688113ee2c7a8363b4399f4b
Vikash Badal <vikashb@where-ever.za.net>
2024-09-15 01:33:21 +0200

    store ESO key as k8s secret

--- roles/docker/files/minikube-init-env.sh
+++ roles/docker/files/minikube-init-env.sh
@@ -41,6 +41,12 @@ kubectl wait -n nfs-provisioning pods \
 FULL_HOSTNAME=$(hostname -f)
 EVT=$(echo ${FULL_HOSTNAME/.*/} | sed 's/u22-//')
 
+kubectl create namespace external-secrets
+kubectl -n external-secrets create secret generic external-hashicorp-vault-token \
+   --from-literal=addr=${EXTERNAL_VAULT_ADDR} \
+   --from-literal=token=${ESO_TOKEN} \
+   --from-file=root.ca=/usr/local/share/ca-certificates/Where_Ever_Root_CA_Root_CA_212425365443306115571426638685908456843.crt
+
 rm -rf gitops
 git clone git@github.com:vikashb72/gitops.git
 helm dep update gitops/charts/argo-cd
@@ -50,9 +56,8 @@ helm install -n argocd argo-cd  gitops/charts/argo-cd \
     --wait 
 
 kubectl -n argocd get secret argocd-initial-admin-secret \
-    -o jsonpath="{.data.password}" | base64 -d; echo
-kubectl -n argocd get secret argocd-initial-admin-secret \
     -o jsonpath="{.data.password}" | base64 -d > argocd.adm.pw
+echo '';cat argocd.adm.pw; echo ''
 argocd login 192.168.49.2:30080 --insecure
 argocd cluster list
 #echo  argocd cluster set in-cluster --name dev-cluster
@@ -60,7 +65,9 @@ argocd cluster list
 helm template gitops/umbrella-chart/${EVT} | kubectl -n argocd apply -f -
 argocd cluster list
 argocd app list
-kubectl create namespace vault-system
-kubectl -n vault-system create secret generic external-vault-unseal \
-    --from-literal=addr=${VAULT_UNSEAL_TOKEN} \
-    --from-literal=token=${VAULT_UNSEAL_ADDR} 
+
+#kubectl create namespace vault-system
+#kubectl -n vault-system create secret generic external-vault-unseal \
+#    --from-literal=addr=${EXTERNAL_VAULT_UNSEAL_TOKEN} \
+#    --from-literal=token=${EXTERNAL_VAULT_ADDR} 
+
